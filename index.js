6061bf0169c3b90fbb60a1eb430025a1a5e3fe45
Revelt <revelt@lc-rp.mp>
2022-06-08 14:08:14 +0700

    ➕ feat(app): file and function documentation

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const fs = require('fs');
+// NodeJS filestream dependency
+const fs = require('fs'); 
+
+// NodeJS readline dependency
 const readline = require('readline');
+
+// Modular functional design js file
 const parkingLotCreate = require('./App/parking-lot/parking-lot-create');
 const parkingLotLeave = require('./App/parking-lot/parking-lot-leave');
 const parkingLotPark = require('./App/parking-lot/parking-lot-park');
 const parkingLotStatus = require('./App/parking-lot/parking-lot-status');
 
+// read line interface with filestream from file_inputs.txt
 const file = readline.createInterface({
     input: fs.createReadStream('./App/parking-lot/file_inputs.txt'),
     output: process.stdout,
     terminal: false
 });
 
+// array slot that has reponsibility to handle all array and allocation memory for parking slot
 let slotArr = [];
 
+// read per line for file_inputs.txt and logical to execute each function per command;
 file.on('line', (line) => {
     const arr = line.split(" ");
     const command = arr[0];
